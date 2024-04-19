01cc8fe222458bfa75e4005ebad30e53259ef8c1
Eduardo Márquez <eduardo20.3263@gmail.com>
2024-04-19 12:25:29 -0400

    Eslint, prettier and stylelint installation and configuration

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,9 +1,10 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Page from "../src/app/page";
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Page from '../src/app/page';
 
 test("Check if the text 'Get started by editing' is defined", () => {
-  render(<Page />);
-  expect(screen.getByText(/Get started by editing/i)).not.toBeNull();
-  // expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
+	expect.hasAssertions();
+	render(<Page />);
+	expect(screen.getByText(/get started by editing/i)).not.toBeNull();
+	// expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
 });
